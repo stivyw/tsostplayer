3b5bebfb8911b16a65357c7fd9c5e59b400959a2
allo.vince <allo.vince@92bcfc9e-8542-11dd-b6e4-1fb4a74862d0>
2009-01-22 01:50:49 +0000

    ver 0.11 beta

--- trunk/avplayer.src.js
+++ trunk/avplayer.src.js
@@ -8,307 +8,235 @@
  * @license    GNU General Public License v3
  * @link       http://code.google.com/p/tsostplayer/
  */
-function AvPlayer(dir,id){
-	var av = this;
+(function(){
 
-	/**
-	 * Page Elements
-	**/
-	this.dir = dir ? dir : './';
+var 
 
-	this.id = id ? id : 'avplayer';
+window = this,
 
-	this.isIE = (navigator.userAgent.match(/MSIE/i));
+undefined ,
 
-	this.getDocument = function(){
-		return (document.body?document.body:(document.documentElement?document.documentElement:document.getElementsByTagName('div')[0]));
-	};
+isIE = (navigator.userAgent.match(/MSIE/i)) ,
 
-	this.container = document.getElementById('avplayer-container')?document.getElementById('avplayer-container'):document.createElement('div');
-
-	/**
-	 * Player Objects
-	**/
-
-	this.melody = '';
-
-	this.lyric = {};
-
-	this.lrcline = 0;
-
-	this.playlist = {};
-
-	/**
-	 * Call flash API
-	**/
-
-	this.player = function(){
-		return document.getElementById(av.id);
+type = function (obj) {
+	if (obj === null) {
+		return 'Null';
 	}
-
-	this._play = function(m){
-		av.player().avPlay(m);
+	if (obj === undefined) {
+		return 'Undefined';
 	}
+	return Object.prototype.toString.call(obj).match(/\s(.+)\]$/)[1];
+},
 
-	this._stop = function(time){
-		time = time > 0 ? time : 0;
-		av.player().avStop(time);
-		av.lrcline = 0;
-	}
+debug = function (m,level){
+	level == undefined ? 0 : level;
+	var type = typeof(m);
+	if(type == 'object') {
+		for(var i in m) {
+			var s = i + ' : ';
+			if(typeof(m[i]) == 'object') {
+				console.log("Js debug : [%s]",s);
+				s = '';
+				p(m[i],level + 1);
+			}
+			else {
+				s = i + ' : ' + m[i];
 
-	this._pause = function(){
-		av.player().avPause();
-	}
-
-	this._status = function(){
-		return av.player().avStatus();
-	}
-
-	this.playthis = function(line){
-		if(av._status().isPlaying == false) {
-			return false;
+			}
+			for(var j = 0; j < level; j++) {
+				s = "\t" + s;
+			}
+			if(s) {
+				console.log("Js debug : [%s]",s);
+			}
 		}
 
-		var time = $(line).attr('class');
-		time = time.match(/\d+/);
-		time = time[0];
-		av._stop(time);
-		av._play();
-
-		$(".avplayLine_hilight").removeClass("avplayLine_hilight");
-		$(line).addClass("avplayLine_hilight");
-		$(".avplayLine:not(.avpLine_" + time + ")").addClass("avplayLine_played");
-		$(".avpLine_" + time +" ~ span").removeClass("avplayLine_played");
 	}
+	else {
+		console.log("Js debug : [%s]",m);
+	}
+},
 
-	/**
-	 * Page Actions
-	 **/ 
-	this.init = function(){
+
+AvPlayer = window.AvPlayer = {
+	
+	info: {
+		loaded : false,
+		version : '0.10'
+	},
+
+
+	option : {
+		id : 'avplayer',
+		dir : './',
+		swf : 'avplayer.swf',
+		css : 'avplayer.css',
+		debug : true
+	},
+
+	container : document.getElementById('avplayer-container')?document.getElementById('avplayer-container'):document.createElement('div'),
+	
+	getDocument : document.body?document.body:(document.documentElement?document.documentElement:document.getElementsByTagName('div')[0]),
+
+	init : function(dir){
+		AvPlayer.option.dir = dir || AvPlayer.option.dir;
 		if (window.addEventListener) {
-			window.addEventListener('load',av.creat,false);
-			window.addEventListener('unload',av.destroy,false);
+			window.addEventListener('load',AvPlayer.creat,false);
+			//window.addEventListener('unload',AvPlayer.destroy,false);
 		} else if (window.attachEvent) {
-			window.attachEvent('onload',av.creat);
-			window.attachEvent('onunload',av.destroy);
+			window.attachEvent('onload',AvPlayer.creat);
+			//window.attachEvent('onunload',AvPlayer.destroy);
 		};
-	}
+	},
 
-	this.creat = function(){
-		var htmlEmbed = '<embed name="'+av.id+'" id="'+av.id+'" src="'+av.dir+'avplayer.swf" width="100%" height="100%" quality="high" allowScriptAccess="always" quality="high" bgcolor="#ffffff" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash"></embed>';
-		var htmlObject = '<object id="'+av.id+'" data="'+av.dir+'avplayer.swf" type="application/x-shockwave-flash" width="100%" height="100%"><param name="movie" value="'+dir+'avplayer.swf" /><param name="AllowScriptAccess" value="always" /><param name="quality" value="high" /><param name="bgcolor" value="#ffffff" /></object>';
-		var html = (!av.isIE?htmlEmbed:htmlObject);
+	setup : function(op){
+		for(var i in op){
+			AvPlayer.option[i] = op[i];
+		}
+	},
+
+	creat : function(){
+		var htmlEmbed = '<embed name="'+AvPlayer.option.id+'" id="'+AvPlayer.option.id+'" src="'+AvPlayer.option.dir+'avplayer.swf" width="100%" height="100%"></embed>';
+		var htmlObject = '<object id="'+AvPlayer.option.id+'" data="'+AvPlayer.option.dir+'avplayer.swf" type="application/x-shockwave-flash" width="100%" height="100%"><param name="movie" value="'+AvPlayer.option.dir+'avplayer.swf" /></object>';
+		var html = !isIE ? htmlEmbed : htmlObject;
+		/*
 		var head = document.getElementsByTagName("head")[0];
 		var css = document.createElement('link');
 		css.type = 'text/css';
 		css.rel = "stylesheet";
-		css.href = av.dir + "avplayer.css";
-		
-		if(!av.container.id){
-			av.container.id = av.id + '-container';
+		css.href = AvPlayer.option.dir + AvPlayer.option.css;
+		*/
+		if(!AvPlayer.container.id){
+			AvPlayer.container.id = AvPlayer.option.id + '-container';
 			var container_style = {position: 'fixed',width: '8px',height: '8px',bottom: '0px',left: '0px',zIndex:-1};
 			for (var x in container_style) {
-				av.container.style[x] = container_style[x];
+				AvPlayer.container.style[x] = container_style[x];
 			}
 		}
 		try{
-			head.appendChild(css);
-			av.getDocument().appendChild(av.container);
-			av.container.innerHTML = html;
+			//head.appendChild(css);
+			AvPlayer.getDocument.appendChild(AvPlayer.container);
+			AvPlayer.container.innerHTML = html;
+			AvPlayer.info.loaded = true;
+			AvPlayer.player = document.getElementById(AvPlayer.option.id);
 		}
 		catch(e){
-			alert(e);
+			debug(e);
 		}
-	}
+	},
 
-	this.destroy = function(){
-		//
-	}
+	play : function(url){
+		if(AvPlayer.info.loaded === false)
+			return debug('Player is not Ready');
+		url = url || null;
+		AvPlayer.player.avPlay(url);	
+	},
 
-	/**
-	 * PlayList functions
-	**/
-	this.addlist = function(){
+	stop : function(time){
+		if(AvPlayer.info.loaded === false)
+			return debug('Player is not Ready');
+		//确保入口参数正确
+		time = type(time) == 'Number' ? time : 0;
+		AvPlayer.player.avStop(time);
+	},
+
+	pause : function(){
+		if(AvPlayer.info.loaded === false)
+			return debug('Player is not Ready');
+		AvPlayer.player.avPause();
+	},
 	
-	}
-
-	this.removelist = function(){
+	next : function(){
+		if(AvPlayer.info.loaded === false)
+			return debug('Player is not Ready');
+		AvPlayer.player.avNext();
+	},
 	
-	}
-
-	this.href2list = function(href){
-		href.addClass('avplayList');
-		href.addClass('avplayList_wait');
-		href.click(function(){
-			var s = av._status();
-			if(s.isPlaying == false && s.played == 0) {
-				av._play($(this).attr('href'));
-				$("#" + av.lyric.id + ' > span .avpLine_' + av.lyric.main[0].time).addClass("avplayLine_hilight");
-				$(this).removeClass('avplayList_wait');
-				$(this).removeClass('avplayList_pause');
-				$(this).addClass('avplayList_now');
-			}
-			if(s.isPlaying == true && s.played > 0) {
-				av._stop(s.played);
-				$(this).removeClass('avplayList_wait');
-				$(this).removeClass('avplayList_now');
-				$(this).addClass('avplayList_pause');
-			}
-			if(s.isPlaying == false && s.played > 0) {
-				av._play();
-				$(this).removeClass('avplayList_wait');
-				$(this).removeClass('avplayList_pause');
-				$(this).addClass('avplayList_now');
-			}
-			return false;
-		});
-	}
-
-	/**
-	 * LRC functions
-	**/
-	function trim(s){
-		return s.replace(/(^\s*)|(\s*$)/g, "");
-	}
-
-	function parseTime(x) {
-		var l = x.split('.');
-		var t = 0;
-		try {
-			var j = l[0].split(':');
-			t += parseInt(j[j.length - 1], 10) * 1000;
-			if (j.length > 1) {
-				t += parseInt(j[j.length - 2], 10) * 60000;
-			}
-			if (j.length > 2) {
-				t += parseInt(j[j.length - 3], 10) * 3600000;
-			}
-			if (l.length > 1) {
-				if (l[1].length == 1) {
-					t += parseInt(l[1], 10) * 100;
-				} else if (l[1].length == 2) {
-					t += parseInt(l[1], 10) * 10;
-				} else if (l[1].length == 3) {
-					t += parseInt(l[1], 10) * 1;
-				}
-			}
-			return t;
-		} catch (e) {
-			return 0;
-		}
-	}
-	function parseLRC(str) {
-		str = str.match(/^\[([0-9:\.]+)\](.*)$/);
-		return str == null ? false : {time:parseTime(str[1]),content:str[2]};
-	}
-
-	function lrc2obj(lrc){
-		var lyric = new Array();
-		lrc = lrc.split(/\n/g);
-		for(var i = 0;i < 4;i++){
-			lrc[i] = trim(lrc[i]);
-			var tmp = lrc[i].match(/\[([(ti)|(ar)|(al)|(by):])(.*)\]/ig);
-			if(tmp == null) {
-				break;
-			}
-			else{
-				switch (tmp[0].substr(1,2)){
-					case 'ti':
-						var title = tmp[0].substr(4,tmp[0].length - 5);
-						break;
-					case 'ar':
-						var artist = tmp[0].substr(4,tmp[0].length - 5);
-						break;
-					case 'al':
-						var album = tmp[0].substr(4,tmp[0].length - 5);
-						break;
-					case 'by':
-						var by = tmp[0].substr(4,tmp[0].length - 5);
-						break;
-					default:;
-				}
-			}
-		}
-		var j = 0;
-		for (i = 0; i < lrc.length; i ++) {
-			lrc[i] = trim(lrc[i]);
-			tmp =  parseLRC(lrc[i]);
-			if(tmp) {
-				lyric[j] = tmp;
-				j++;
-			}
-		}
-		return {title:title,artist:artist,album:album,by:by,main:lyric};
-	}
-
-	function printLyric(lyric) {
-		var res = '';
-		for(var i = 0;i < lyric.length;i++) {
-			//res += '<span class="avplayLine avpLine_'+ lyric[i].time +'" onclick="alert(avp);">' + lyric[i].content + "</span>\n";
-			res += '<p class="avplayLine avpLine_'+ lyric[i].time +'" onclick="avp.playthis(this);">' + lyric[i].content + "</p>\n";
-		}
-		return res;
-	}
-
-	this.initlrc = function(pre){
-		av.lyric = lrc2obj(pre.html());
-		var id = "avLrc" + Math.floor(Math.random()*10000);
-		av.lyric.id = id;
-		pre.replaceWith('<div id="' + id + '" class = "avplayLrcCase">' + printLyric(av.lyric.main) + '</div>');
-		//return pre;
-	}
-
-	this.addlyc = function(){
+	prev : function(){
+		if(AvPlayer.info.loaded === false)
+			return debug('Player is not Ready');
+		AvPlayer.player.avPrev();
+	},
 	
-	}
+	reset : function(){
+		if(AvPlayer.info.loaded === false)
+			return debug('Player is not Ready');
+		AvPlayer.player.avReset();
+	},
 
-	this.removelyc = function(){
+	status : function(){
+		return AvPlayer.player.avStatus();
+	},
 	
+	melody : function(){
+		if(AvPlayer.info.loaded === false)
+			return debug('Player is not Ready');
+		AvPlayer.player.avSetMelody();
+	},
+
+	list : function(url){
+		if(AvPlayer.info.loaded === false)
+			return debug('Player is not Ready');
+		if(url === undefined)
+			return AvPlayer.player.avGetList()
+		AvPlayer.player.avSetList(url);
+	},
+	
+	lyric : function(lrc){
+		if(AvPlayer.info.loaded === false)
+			return debug('Player is not Ready');
+		if(lrc === undefined)
+			return AvPlayer.player.avGetLyric()
+		AvPlayer.player.avSetLyric(lrc);
+	},
+	
+	PlayComplete : function(){
+				   
+	},
+	
+	PlayShowLyric : function(l) {
+		//p(l.prevLyric);
 	}
 
-	/**
-	 * Called by swf
-	**/
 
-	this.EnterFrame = function(s){
-		var added = false;
-		if(s < av.lyric.main[av.lrcline].time) {
-			if(added == false) {
-				var now = av.lrcline - 1 >= 0 ? av.lrcline - 1 : 0;
-				//$('.avpLine_' + avLyric.lyric[now].time).addClass("avplayLine_hilight").highlightFade({speed:1000});
-				//$("#"+ avLyric.id).scrollTo($('.avpLine_' + avLyric.lyric[now].time),{offset:-150});
-				$('.avpLine_' + av.lyric.main[now].time).addClass("avplayLine_hilight");
-				av.lrcline > 0 ? $('.avpLine_' + av.lyric.main[now - 1].time).removeClass("avplayLine_hilight").addClass("avplayLine_played") : '';
+};
+
+})();
+
+
+
+AvPlayer.init('../');
+
+
+function p(m,level){
+	level == undefined ? 0 : level;
+	var type = typeof(m);
+	if(type == 'object') {
+		for(var i in m) {
+			var s = i + ' : ';
+			if(typeof(m[i]) == 'object') {
+				console.log("Js debug : [%s]",s);
+				s = '';
+				p(m[i],level + 1);
+			}
+			else {
+				s = i + ' : ' + m[i];
+
+			}
+			for(var j = 0; j < level; j++) {
+				s = "\t" + s;
+			}
+			if(s) {
+				console.log("Js debug : [%s]",s);
 			}
 		}
-		else {
-			added = true;
-			av.lrcline++;
-		}
+
 	}
-
-	this.IoError = function(){
-		$('.avplayList_now').removeClass("avplayList_now").addClass("avplayList_error");
-		$('.avplayLine_hilight').removeClass("avplayLine_hilight");
+	else {
+		console.log("Js debug : [%s]",m);
 	}
-
-	this.PlayComplete = function(){
-		$('.avplayList_now').removeClass("avplayList_now").addClass('avplayList_wait');
-		$('.avplayLine_hilight').removeClass("avplayLine_hilight");
-		$('.avplayLine_played').removeClass("avplayLine_played");
-	}
-
-	/**
-	 * constractor
-	 **/
-	av.init();
-	$("a[href$='.mp3']").each(function(){
-		av.href2list($(this));
-	});
-	$("pre[class='lrc']").each(function(){
-		av.initlrc($(this));
-	});
-
 }
+
+
 
 -->
---
+++ trunk/avplayerUI.src.js
@@ -0,0 +1,16 @@
+<!--
+/**
+ * The AVPlayer Javascript Interface UI
+ *
+ *
+ * @author     AlloVince <allo.vince@gmail.com>
+ * @copyright  2008-2009 AVE7.NET
+ * @license    GNU General Public License v3
+ * @link       http://code.google.com/p/tsostplayer/
+ */
+(function(){
+
+
+})();
+
+-->
